550db44cc6a3a7d0ce2adc78a8231a5f4ae601ed
t-mangoe <m.takashi.xpair@gmail.com>
2017-05-04 00:16:50 +0900

    mechanizeを使った実装に変更

--- nico_seige-dl.rb
+++ nico_seige-dl.rb
@@ -1,49 +1,26 @@
 #!/usr/bin/env ruby
+
 require "nokogiri"
-require "open-uri"
-require "net/https"
+require 'mechanize'
 
-def login(mail, pass)
-  host = 'secure.nicovideo.jp'
-  path = '/secure/login?site=niconico'
-  body = "mail=#{mail}&password=#{pass}"
-
-  https             = Net::HTTP.new(host, 443)
-  https.use_ssl     = true
-  https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  response          = https.start { |https|
-    https.post(path, body)
-  }
-
-  cookie = ''
-  response['set-cookie'].split('; ').each do |st|
-    if idx=st.index('user_session_')
-      cookie = "user_session=#{st[idx..-1]}"
-      break
-    end
-  end
-
-  return cookie
-end
+LOGIN = 'https://secure.nicovideo.jp/secure/login?site=niconico'
+agent = Mechanize.new
 
 mail = "mailaddress" #write your mailaddress of niconico account
 pass = "password" #write your password of niconico account
+agent.post(LOGIN, 'mail' => mail, 'password' => pass)
+
 id = 961724
 sourceURL = "http://seiga.nicovideo.jp/image/source/"
 seiga_url = "http://seiga.nicovideo.jp/seiga/"
-cookie = login(mail,pass)
 
 url = sourceURL + id.to_s
+data = agent.get(url)
 
-charset = nil
-html = open(url,{"Cookie" => cookie}) do |f|
-	charset = f.charset
-	f.read
-end
-
-doc = Nokogiri::HTML.parse(html, nil, charset)
+doc = Nokogiri::HTML.parse(data.body)
 illust = doc.css("div.illust_view_big")
 img_src = illust.css("img").attr("src")
 img_URL = "http://lohas.nicoseiga.jp" + img_src.to_s
-title = doc.title
+title = doc.title.split
+p title
 `wget -O #{title[0]}.jpg -nc #{img_URL}`
